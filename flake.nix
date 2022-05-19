5756e4e1d096541ed3b3043dc5696ab878d2d178
harris-chris <chrisharriscjh@gmail.com>
2022-05-19 14:33:28 +0900

    Add kakoune-workspace to flake

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     flake-utils.url = github:numtide/flake-utils;
     signal-desktop.url = path:./signal-desktop-compat;
     get-workspace-name.url = path:./get-workspace-name;
+    kakoune-workspace.url = path:./kakoune-workspace;
   };
   outputs = {
     self
@@ -11,6 +12,7 @@
     , flake-utils
     , signal-desktop
     , get-workspace-name
+    , kakoune-workspace
   }:
     flake-utils.lib.eachDefaultSystem (system:
       let
@@ -18,5 +20,6 @@
       in rec {
         packages.signal-desktop = signal-desktop.defaultPackage.${system};
         packages.get-workspace-name = get-workspace-name.defaultPackage.${system};
+        packages.kakoune-workspace = kakoune-workspace.defaultPackage.${system};
       });
 }
