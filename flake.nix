02e6e1d00898b5546a833478f6e29bc3a8f23a2b
harris-chris <chrisharriscjh@gmail.com>
2022-05-19 14:22:49 +0900

    Adding back in get-workspace-name

--- flake.nix
+++ flake.nix
@@ -3,17 +3,20 @@
     nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
     flake-utils.url = github:numtide/flake-utils;
     signal-desktop.url = path:./signal-desktop-compat;
+    get-workspace-name.url = path:./get-workspace-name;
   };
   outputs = {
     self
     , nixpkgs
     , flake-utils
     , signal-desktop
+    , get-workspace-name
   }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in rec {
         packages.signal-desktop = signal-desktop.defaultPackage.${system};
+        packages.get-workspace-name = get-workspace-name.defaultPackage.${system};
       });
 }
