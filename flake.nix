f1787e8a76e56861fa870e3ec5be13ebfbc343f3
harris-chris <chrisharriscjh@gmail.com>
2023-07-17 09:59:19 +0900

    Add overlay

--- flake.nix
+++ flake.nix
@@ -15,18 +15,13 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in rec {
-        packages = flake-utils.lib.flattenTree {
+        getPersonalPackages = pkgs: flake-utils.lib.flattenTree {
           get-workspace-name = get-workspace-name.defaultPackage.${system};
           kakoune-workspace = kakoune-workspace.defaultPackage.${system};
           signal-desktop = pkgs.signal-desktop;
         };
-        # REMEMBER YOU MAY NEED TO NIX FLAKE UPDATE AS WELL FOR DEVSHELL
-        devShell = pkgs.mkShell {
-          buildInputs = builtins.attrValues packages;
-        };
-        overlays = final: prev: {
-          inherit (packages) get-workspace-name kakoune-workspace signal-desktop;
-        };
+      in {
+        packages = getPersonalPackages pkgs;
+        overlays = final: prev:  getPersonalPackages prev;
       });
 }
