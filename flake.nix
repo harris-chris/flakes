e3170bdd242c7c385a688666c92004dbbcd9ed0e
harris-chris <chrisharriscjh@gmail.com>
2023-07-18 10:35:55 +0900

    remove get-workspace-name kakoune-workspace

--- flake.nix
+++ flake.nix
@@ -1,24 +1,16 @@
 {
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    get_workspace_name.url = github:harris-chris/get-workspace-name;
-    kakoune_workspace.url = github:harris-chris/kakoune-workspace;
   };
   outputs = {
     self
     , nixpkgs
-    , get_workspace_name
-    , kakoune_workspace
   }:
   let
     system = "x86_64-linux";
 
     pkgs = import nixpkgs {
       inherit system;
-      overlays = [
-        get_workspace_name.overlays.default
-        kakoune_workspace.overlays.default
-      ];
     };
 
     openfortivpn = (import (builtins.fetchGit {
@@ -28,10 +20,8 @@
       rev = "8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8";
     }) { inherit system; }).openfortivpn;
 
-    getPersonalPackages = pkgs: {
+    getPersonalPackages = _: {
       inherit openfortivpn;
-      getworkspacename = pkgs.getworkspacename;
-      kakoune-workspace = pkgs.kakoune-workspace;
       signal-desktop = pkgs.signal-desktop;
     };
   in {
